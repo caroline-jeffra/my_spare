3f98885fd33c45b5228177b410d3e582842ce311
Thiago Souza <thlucioxi@gmail.com>
2023-08-21 17:08:15 +0200

    Correct routes for dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
-  resources :dashboards, only: %i[show]
+  get "dashboards/", to: "dashboards#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
