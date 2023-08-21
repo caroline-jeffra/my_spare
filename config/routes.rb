baf0a28e30da9a38427702d2e94519082a10da4c
Thiago Souza <thlucioxi@gmail.com>
2023-08-21 17:04:19 +0200

    Create routes, show action and view on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  resources :dashboards, only: %i[show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
