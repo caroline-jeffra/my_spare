efdc7fc61a34c4954c7f399a9e4a081e4402c78d
Thiago Souza <thlucioxi@gmail.com>
2023-08-21 22:52:05 +0200

    Add some structure to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Cleaning database..."
+Booking.destroy_all
+Instrument.destroy_all
+User.destroy_all
+
+puts "Introducing music to humankind (creating users)"
+
+# The following are normal users
+
+7.times do
+  User.create(email: xxx, encrypted_password: "password", first_name: xxx, last_name: xxx, city: xxx)
+end
+
+# The following are instrument owners
+user1 = User.create(email: xxx, encrypted_password: "password", first_name: xxx, last_name: xxx, city: xxx)
+
+Faker::Book.title
+
+puts "Users created"
+
+puts "Creating instruments"
+
+instrument1 = Instrument.create(model: xxx, brand: xxx, category: xxx, daily_price: xxx, description: xxx, image_url: xxx, user_id: user1.id)
+
+
+puts "Instruments created and tuned!"
+
+# If need, the code below can be used to seed Bookings
+
+puts "Booking some instruments"
+
+Booking.create(start_date: xxx, end_date: xxx, cost: xxx, instrument_id: instrument1.id, user_id: user1.id)
+
+
+puts "Part of the available instruments were booked!"
