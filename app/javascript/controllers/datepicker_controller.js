0f5e8143f82a648eef6c974e9a4f34edfdaeea07
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-08-22 15:57:10 +0200

    start booking form create

---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
+
+// Connects to data-controller="datepicker"
+export default class extends Controller {
+  connect() {
+    flatpickr(this.element)
+  }
+}
+
+
+// import { Controller } from "@hotwired/stimulus"
+
+// export default class extends Controller {
+//   connect() {
+//   }
+// }
