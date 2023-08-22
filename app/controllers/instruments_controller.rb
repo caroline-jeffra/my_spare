770d8b02f2ff15dc499f99c4b9a28003075e841c
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-22 14:32:56 +0200

    added new and create

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,14 +1,24 @@
 class InstrumentsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new edit]
   def index
+    @instruments = search_params
+    raise
   end
 
   def show
   end
 
   def new
+    @instrument = Instrument.new
   end
 
   def create
+    @instrument = Instrument.new(instrument_params)
+    if @instrument.save!
+      redirect_to instrument_path(@instrument)
+    else
+      render :new_instrument, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +28,24 @@ class InstrumentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument)
+      .permit(
+        :model,
+        :brand,
+        :category,
+        :daily_price,
+        :description,
+        :image_url)
+      .merge(user: current_user)
+  end
+
+  # for future use
+  def search_params
+    params.require(:instrument).permit(:model, :brand, :category)
   end
 end
