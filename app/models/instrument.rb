75f40b208b6606e108ff75842e62cffcb26d063a
Nathan Swulinski nswulinski@gmail.com <nswulinski@gmail.com>
2023-08-23 16:36:41 +0200

    improvements

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,8 @@
 class Instrument < ApplicationRecord
   belongs_to :user
 
+  has_many :bookings
+
   validates :category, :daily_price, :image_url, :city, presence: true
   validates :daily_price, numericality: { greater_than: 0 }
 end
