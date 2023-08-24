cc76403de8f9c51248425372fa01a4b32cd765b5
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-24 20:07:34 +0200

    refactored code

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -13,35 +13,48 @@ class Booking < ApplicationRecord
   # Returns array of Bookings that would overlap with dates
   # If the array is empty there are no conflicts
   # If the array is not empty there are conflicts
-  def self.overlapping(new_booking_start_date, new_booking_end_date, new_booking_instrument_id)
-    where "
-    instrument_id = :instrument_id AND (
-      (
-        start_date <= :start_date AND
-        end_date >= :start_date
-      ) OR (
-        start_date <= :end_date AND
-        end_date >= :end_date
-      )
-    )", start_date: new_booking_start_date, end_date: new_booking_end_date, instrument_id: new_booking_instrument_id
-  end
+  # def self.overlapping(new_booking_start_date, new_booking_end_date, new_booking_instrument_id)
+  #   where "
+  #   instrument_id = :instrument_id AND 
+  #   (
+  #     (
+  #       start_date <= :start_date AND
+  #       end_date >= :start_date
+  #     ) OR (
+  #       start_date <= :end_date AND
+  #       end_date >= :end_date
+  #     )
+  #   )", start_date: new_booking_start_date, end_date: new_booking_end_date, instrument_id: new_booking_instrument_id
+  # end
 
-  def self.unavailable(new_booking_start_date, new_booking_end_date)
-    where "
-    (
-      start_date <= :start_date AND
-      end_date >= :start_date
-    ) OR (
-      start_date <= :end_date AND
-      end_date >= :end_date
-    )", start_date: new_booking_start_date, end_date: new_booking_end_date
+  # def self.unavailable(new_booking_start_date, new_booking_end_date)
+  #   where "
+  #   (
+  #     start_date <= :start_date AND
+  #     end_date >= :start_date
+  #   ) OR (
+  #     start_date <= :end_date AND
+  #     end_date >= :end_date
+  #   )", start_date: new_booking_start_date, end_date: new_booking_end_date
+  # end
+
+  def self.overlapping(new_booking_start_date, new_booking_end_date)
+    where("
+          (
+            :start_date BETWEEN start_date AND end_date
+          ) OR (
+            :end_date BETWEEN start_date AND end_date
+          ) OR (
+            start_date >= :start_date AND end_date <= :end_date
+          )
+          ", start_date: new_booking_start_date, end_date: new_booking_end_date)
   end
 
   private
 
   def no_booking_overlap
     # Guard clause
-    return true if Booking.overlapping(start_date, end_date, instrument_id).none?
+    return true if instrument.bookings.overlapping(start_date, end_date).none?
 
     # else
     errors.add(:start_date, 'it overlaps another reservation')
