c3dddf790b4fe26cd02483e2a8112d8fa1fd6ad1
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-24 22:07:41 +0200

    improved calendar

--- app/javascript/controllers/datesearch_controller.js
+++ app/javascript/controllers/datesearch_controller.js
@@ -2,8 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
 export default class extends Controller {
+  static targets = ['calendar']
+
   connect() {
-    flatpickr(this.element, {})
+    flatpickr(this.calendarTarget, {
+      minDate: "today"
+    })
+  }
+
+  toggle() {
+    if(this.calendarTarget.classList.contains("isOpen")) {
+      flatpickr(this.calendarTarget, {
+        minDate: "today"
+      }).close()
+      this.calendarTarget.classList.remove("isOpen")
+    } else {
+      flatpickr(this.calendarTarget, {
+        minDate: "today"
+      }).open()
+      this.calendarTarget.classList.add("isOpen")
+    }
+  }
+
+  clear() {
+    flatpickr(this.calendarTarget, {
+      minDate: "today"
+    }).clear()
   }
 }
 
