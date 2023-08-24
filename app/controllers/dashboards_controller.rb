8b01d059d94037afb978c371afcb1dfaa9cede02
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-08-24 16:47:00 +0200

    basic your instruments styling with supplementary functionality needed

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,13 +7,12 @@ class DashboardsController < ApplicationController
     # # You can get the current user by running current_user
     # # You could use Instrument.where to filter the instruments
     @instruments = current_user.instruments
-    
+
     # @bookings must only be the current user's bookings
     # # You can get the current user by running current_user
     # # You could use Booking.where to filter the Bookings
     @bookings = current_user.bookings
 
     # In the dashboard html, display this information, following the sketch
-
   end
 end
