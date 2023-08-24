94c1063d595a95b246419a85c80150022cb20457
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-08-24 12:18:18 +0200

    disable booked dates from flatpickr on instrument show page

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,7 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
 export default class extends Controller {
+  static values = {
+    bookings: Array
+  }
+
   connect() {
-    flatpickr(this.element)
+    console.log(this.bookingsValue)
+    flatpickr(this.element, {
+      dateFormat: "Y-m-d",
+      disable: this.bookingsValue
+    })
   }
 }
