d67e5c6380ff3382229fec0f72532f7e8cb4f1be
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-24 12:10:25 +0200

    added bookings YMD function

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -12,6 +12,12 @@ class Instrument < ApplicationRecord
   validates :category, :daily_price, :image_url, :city, presence: true
   validates :daily_price, numericality: { greater_than: 0 }
 
+  def bookings_strings
+    bookings.pluck(:start_date, :end_date).map do |i|
+      [i.first.strftime("%Y-%m-%d"), i.last.strftime("%Y-%m-%d")]
+    end
+  end
+
   geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_city?
 end
