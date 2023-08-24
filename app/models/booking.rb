6fa19d5d917210babbb8f525d6170a0080168751
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-24 20:11:46 +0200

    removed old code

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,35 +9,6 @@ class Booking < ApplicationRecord
   # calls no_booking_overlap method
   validate :no_booking_overlap
 
-  # Runs Booking.where
-  # Returns array of Bookings that would overlap with dates
-  # If the array is empty there are no conflicts
-  # If the array is not empty there are conflicts
-  # def self.overlapping(new_booking_start_date, new_booking_end_date, new_booking_instrument_id)
-  #   where "
-  #   instrument_id = :instrument_id AND 
-  #   (
-  #     (
-  #       start_date <= :start_date AND
-  #       end_date >= :start_date
-  #     ) OR (
-  #       start_date <= :end_date AND
-  #       end_date >= :end_date
-  #     )
-  #   )", start_date: new_booking_start_date, end_date: new_booking_end_date, instrument_id: new_booking_instrument_id
-  # end
-
-  # def self.unavailable(new_booking_start_date, new_booking_end_date)
-  #   where "
-  #   (
-  #     start_date <= :start_date AND
-  #     end_date >= :start_date
-  #   ) OR (
-  #     start_date <= :end_date AND
-  #     end_date >= :end_date
-  #   )", start_date: new_booking_start_date, end_date: new_booking_end_date
-  # end
-
   def self.overlapping(new_booking_start_date, new_booking_end_date)
     where("
           (
