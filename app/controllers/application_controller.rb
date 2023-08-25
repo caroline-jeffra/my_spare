a10ce5cc7162f05914cf33fe221b605c2052be35
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-25 09:40:26 +0200

    cleaned up App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  # before_action :set_user_coordinates
-  # private
-
-  # def set_user_coordinates
-  #   @user_coordinates = current_user.c
-  # end
 end
